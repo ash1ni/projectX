3bc27836c00fa4b334af9eea748e87f288760cd8
Kumar Tatsat <kumar.tatsat@loconav.com>
2023-04-14 01:48:13 +0530

    Bug fixes and enhancements (#12)
    * update gitignore
    
    * add niceness to html
    
    * lots of stuff
    
    - update task counter when removing elements
    - fix bug in clear all completed tasks button where it would not remove
      multiple tasks that are marked in succession

--- index.js
+++ index.js
@@ -25,14 +25,15 @@ function createNewTask(taskString) {
   task.classList.add("task");
   let taskID = `task-${TASKS_COUNT++}`;
 
-  let inputElem = document.createElement("input");
-  inputElem.setAttribute("type", "checkbox");
-  inputElem.id = taskID;
+  let inputElement = document.createElement("input");
+  inputElement.setAttribute("type", "checkbox");
+  inputElement.id = taskID;
+  inputElement.addEventListener('change', addActionMarkTaskAsCompleted);
 
   let taskLabel = createTaskLabel(taskID, taskString);
   let deleteTaskButton = createDeleteTaskButton();
 
-  task.appendChild(inputElem);
+  task.appendChild(inputElement);
   task.appendChild(taskLabel);
   task.appendChild(deleteTaskButton);
 
@@ -76,28 +77,45 @@ function deleteTaskListener(ev) {
 
 function clearCompletedTasks() {
   CLEAR_LIST_BTN.addEventListener('click', (ev) => {
-    ev.stopPropagation();
-    
-    let tasks = document.querySelector(".tasks").childNodes;
+    ev.preventDefault();
+    ev.stopImmediatePropagation();
 
-    console.log(`tasks: ${tasks}`);
+    let allTasks = document.getElementsByClassName('tasks')[0].querySelectorAll('.task');
 
-    for (let i=0; i<tasks.length;i++) {
-      let task = tasks[i].childNodes;
+    // make a list of nodes which are to be removed
+    // remove them all at once after the iteration is done
+    let tasksToBeRemoved = [];
+    for (let i = 0; i < allTasks.length; i++) {
+      let currentTask = allTasks[i];
 
-      if (task[0].checked) {
-        TASKS_SECTION.removeChild(tasks[i]);
-      } 
-      console.log(`tasks[i]: ${tasks[i]}`);
+      // children[0] contains checkbox element
+      if (currentTask.children[0].checked) {
+        tasksToBeRemoved.push(currentTask);
+      }
+    }
+
+    for (let task of tasksToBeRemoved) {
+      TASKS_SECTION.removeChild(task);
     }
   });
 }
-
-clearCompletedTasks();
 
 function updateTaskCount() {
   TASK_COUNT_ELEMENT.textContent = `${TASKS_COUNT} tasks remaining`;
 }
 
+function addActionMarkTaskAsCompleted(ev) {
+  let checkBox = ev.target;
+
+  if (checkBox.checked) {
+    TASKS_COUNT -= 1;
+    updateTaskCount();
+  } else {
+    TASKS_COUNT += 1;
+    updateTaskCount();
+  }
+}
+
+clearCompletedTasks();
 addActionNewTaskButton();
 updateTaskCount();
